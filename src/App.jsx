54a9423120ac171bbc53c5661f83e9c37d03f7ac
Tim Bryant <timcalvin@gmail.com>
2022-10-21 17:44:34 -0400

    Finish More Components Chapter
    - finish more components chapter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const list = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+    author: 'Jordan Walker',
     num_comments: 3,
     points: 4,
     objectID: 0,
@@ -19,30 +19,44 @@ const list = [
   },
 ]
 
+function List() {
+	return (
+		<ul>
+			{list.map(function (item) {
+				return (
+					<li key={item.objectID}>
+						<span>
+							<a href={item.url}>{item.title}</a>
+						</span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
 }
