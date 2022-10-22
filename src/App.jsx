54c9c2486e151d3998f95bbea24cc82435b83c12
Tim Bryant <timcalvin@gmail.com>
2022-10-22 01:10:13 -0400

    Complete sectioni 10
    - refactor code to use arrow function expressions

--- src/App.jsx
+++ src/App.jsx
@@ -19,46 +19,40 @@ const list = [
   },
 ]
 
-function List() {
-	return (
-		<ul>
-			{list.map(function (item) {
-				return (
-					<li key={item.objectID}>
-						<span>
-							<a href={item.url}>{item.title}</a>
-						</span>
-						<span>{item.author}</span>
-						<span>{item.num_comments}</span>
-						<span>{item.points}</span>
-					</li>
-				);
-			})}
-		</ul>
-	);
-}
+const List = () => (
+	<ul>
+		{list.map((item) => (
+			<li key={item.objectID}>
+				<span>
+					<a href={item.url}>{item.title}</a>
+				</span>
+				<span>{item.author}</span>
+				<span>{item.num_comments}</span>
+				<span>{item.points}</span>
+			</li>
+    ))}
+	</ul>
+);
 
-function Search() {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  );
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
 
-      <Search />
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <hr />
+    <Search />
 
-      <List />
-    </div>
-  );
-}
+    <hr />
+
+    <List />
+  </div>
+);
+
 
 export default App;
