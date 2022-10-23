1ad1038da29386744e8213a58347c9d4d522334a
Tim Bryant <timcalvin@gmail.com>
2022-10-23 00:41:17 -0400

    Finish section 11
    - add event handler to Search component.

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,21 @@ const List = () => (
 	</ul>
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
 
 
 const App = () => (
